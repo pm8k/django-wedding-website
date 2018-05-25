07e9d411e9e69d7689dc6f1e9bcbe3c6bf889bad
Matthew Russell <astromars42@gmail.com>
2018-05-24 19:25:21 -0500

    added migration to change type size

--- guests/models.py
+++ guests/models.py
@@ -30,7 +30,7 @@ class Party(models.Model):
     A party consists of one or more guests.
     """
     name = models.TextField()
-    type = models.CharField(max_length=10, choices=ALLOWED_TYPES)
+    type = models.CharField(max_length=100, choices=ALLOWED_TYPES)
     category = models.CharField(max_length=20, null=True, blank=True)
     # save_the_date_sent = models.DateTimeField(null=True, blank=True, default=None)
     # save_the_date_opened = models.DateTimeField(null=True, blank=True, default=None)
